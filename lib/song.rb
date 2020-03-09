72ef54b926b6a87127e2d6c71c735d8d11a3f500
Melissa Sperry <sperry.mel@gmail.com>
2020-03-09 04:41:01 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,21 +1,5 @@
-def add_song(song)
-  self.songs << song
-  song.artist = self
-  @@song_count +=1
-end
-
-def add_song_by_name(name)
-  song = Song.new(name)
-  self.songs << song
-  song.artist = self
-  @@song_count +=1
-end
-
-def songs
-  @songs
-end
-
-def self.song_count
-  @@song_count
-end
+class Song
+  
+  attr_accessor :title, :artist
+  
 end
